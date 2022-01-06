c6ae07743d9e7c914472346c7a24a7ea66db4011
Paweł Brzozowski <mac@192.168.1.4>
2022-01-06 23:19:24 +0100

    added canvasView / toolpicker

--- Draftsman/MainViewController.swift
+++ Draftsman/MainViewController.swift
@@ -6,15 +6,40 @@
 //
 
 import UIKit
+import PencilKit
+import PhotosUI
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
 
+    let canvasView = PKCanvasView()
+
+    // Place to save drawning.
+    var drawning =  PKDrawing()
+    
+    let toolPicker = PKToolPicker.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //Setup naviagtion item
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(leftBarButtonTapped))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(rightBarButtonTapped))
+        
+        // Setup the canvasView
+        canvasView.delegate = self
+        canvasView.drawing = drawning // set drawning property to our drawning obejct creted
+        canvasView.alwaysBounceVertical = true
+        canvasView.drawingPolicy = .anyInput
+        
+        // Setup the toolpicker
+        toolPicker.addObserver(canvasView)
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        canvasView.becomeFirstResponder()
+        canvasView.backgroundColor = .clear
+        canvasView.isOpaque = false
+        
+        // Setupview
+        self.view.addSubview(canvasView)
     }
     
 
